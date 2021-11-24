bfcbcaad66746c01a491edb379fbef6e03839699
Jean Castillo <jean.castillo@devsu.com>
2021-11-24 14:50:28 -0500

    simple module with greetings created

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  hello: function () {
+    console.log("Hello World");
+  },
+  customHello: function (name) {
+    console.log(`Hi ${name}`);
+  },
+  helloNPMCourse: function () {
+    console.log("Hello to NPM Course!");
+  },
+};
